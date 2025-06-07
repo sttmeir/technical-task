c4d34d38d4d35d16bc82b1294543a7ef8abc4894
sttmeir <jasonparser64@gmail.com>
2025-06-07 22:16:06 +0500

    Added create endpoint to client controller

--- src/main/java/kz/sto/car_service/controller/ClientController.java
+++ src/main/java/kz/sto/car_service/controller/ClientController.java
@@ -1,4 +1,23 @@
 package kz.sto.car_service.controller;
 
+import kz.sto.car_service.domain.Client;
+import kz.sto.car_service.repository.ClientRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/clients")
+@RequiredArgsConstructor
 public class ClientController {
+
+    private final ClientRepository clientRepository;
+
+    @PostMapping("/create")
+    public Client create(@RequestParam String name,
+                         @RequestParam String phoneNumber) {
+        return clientRepository.save(new Client(null, name, phoneNumber));
+    }
 }
